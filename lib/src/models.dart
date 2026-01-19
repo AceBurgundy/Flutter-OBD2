cd45ae59ac09e1b3726b6a291c7e0b0b726ceca6
AceBurgundy <Samadriansabalo99@gmail.com>
2026-01-19 22:50:31 +0800

    Removed unused models.

--- lib/src/models.dart
+++ lib/src/models.dart
@@ -1,9 +1,9 @@
-class PIDInformation {
+class DetailedPID {
   final String parameterID;
   final String name;
   final String formula;
 
-  const PIDInformation(this.parameterID, this.name, this.formula);
+  const DetailedPID(this.parameterID, this.name, this.formula);
 }
 
 abstract class TelemetryValue<T> {
@@ -11,50 +11,4 @@ abstract class TelemetryValue<T> {
   final DateTime timestamp;
 
   TelemetryValue(this.value) : timestamp = DateTime.now();
-}
-
-class DTCInformation {
-  final String code;
-  final String description;
-
-  const DTCInformation(this.code, this.description);
-}
-
-class DiagnosticInfo {
-  final String code;
-  final String description;
-  final String severity;
-  final List<String> possibleCauses;
-
-  const DiagnosticInfo(this.code, this.description, this.severity, this.possibleCauses);
-}
-
-class TelemetryData {
-  final List<TelemetryValue<dynamic>> values;
-
-  TelemetryData(this.values);
-}
-
-class VehicleInfo {
-  final String vin;
-  final String make;
-  final String model;
-  final int year;
-
-  const VehicleInfo(this.vin, this.make, this.model, this.year);
-}
-
-class DTCHistory {
-  final DTCInformation dtc;
-  final DateTime detectedTime;
-  final bool isActive;
-
-  const DTCHistory(this.dtc, this.detectedTime, this.isActive);
-}
-
-class EngineData extends TelemetryValue<double> {
-  final String parameterID;
-  final String name;
-
-  EngineData(super.value, this.parameterID, this.name);
 }

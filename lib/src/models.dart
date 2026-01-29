d1bc949715dd5b484666ed3f9e2d4a80b9739171
AceBurgundy <Samadriansabalo99@gmail.com>
2026-01-29 23:06:24 +0800

    Added obd2QueryReturnType and some documentation.

--- lib/src/models.dart
+++ lib/src/models.dart
@@ -1,8 +1,26 @@
-class DetailedPID {
-  final String standardID;
+import 'package:obd2/obd2.dart';
+
+/// A structured representation of a single OBD-II Parameter ID (PID).
+///
+/// The generic type [T] defines the data type returned by this PID.
+/// - `DetailedPID<double>`: Returns a double (e.g. RPM).
+/// - `DetailedPID<String>`: Returns a String (e.g. VIN).
+/// - `DetailedPID<List<double>>`: Returns a list (e.g. Lambda).
+class DetailedPID<T> {
+  final String standard;
   final String parameterID;
   final String name;
   final String formula;
+  final OBD2QueryReturnValue obd2QueryReturnType;
 
-  const DetailedPID(this.standardID, this.parameterID, this.name, this.formula);
+  const DetailedPID(
+      this.standard,
+      this.parameterID,
+      this.name,
+      this.formula, {
+        this.obd2QueryReturnType = OBD2QueryReturnValue.double,
+      });
+
+  @override
+  String toString() => "$name ($parameterID)";
 }

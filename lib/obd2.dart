3484750c1cd02380b604bb46f0465f746add2b79
AceBurgundy <Samadriansabalo99@gmail.com>
2026-01-29 23:04:26 +0800

    Exported Telemetry Data.

--- lib/obd2.dart
+++ lib/obd2.dart
@@ -5,8 +5,10 @@ export 'src/core/diagnostic_standards/sae_j1979/standard.dart';
 export 'src/core/diagnostic_standards/sae_j1979/modes/mode_01.dart';
 export 'src/core/diagnostic_standards/standard_abstract.dart';
 export 'src/core/diagnostic_standards/modes_abstract/mode_01.dart' show TelemetrySession;
+export 'src/core/diagnostic_standards/modes_abstract/mode_01.dart' show TelemetryData;
 export 'src/conversions.dart';
 export 'src/functions.dart';
 export 'src/models.dart';
+export 'src/enums.dart';
 
 // TODO: Export any libraries intended for clients of this package.
